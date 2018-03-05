9550ab543be14997e966e59a43d9eec027d2f5cb
qneumiiller <code@getbraintree.com>
2018-03-05 23:27:13 +0000

    publish.sh suggestions
    Remove semicolons
    set -u for undefined variables

--- publish.sh
+++ publish.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -e;
+set -e
+set -u
 
 if ! git diff-files --quiet; then
-    echo "Can not publish with unstaged uncommited changes";
-    exit 1;
-fi;
+    echo "Can not publish with unstaged uncommited changes"
+    exit 1
+fi
 
 if ! git diff-index --quiet --cached HEAD; then
-    echo "Can not publish with staged uncommited changes";
-    exit 1;
-fi;
+    echo "Can not publish with staged uncommited changes"
+    exit 1
+fi
 
 npm version patch
 
-git push;
-git push --tags;
-npm publish;
+git push
+git push --tags
+npm publish
