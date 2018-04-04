d26b3a9eef19d698f51de51fc6bcfe620553a308
Daniel Brain <dbrain@paypal.com>
2018-04-03 21:47:22 -0700

    Fix karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,13 @@
 /* @flow */
 
 import { getKarmaConfig } from 'grumbler-scripts/config/karma.conf';
-
-import { WEBPACK_CONFIG } from './webpack.config';
+import { getWebpackConfig } from 'grumbler-scripts/config/webpack.config';
 
 export default function configKarma(karma : Object) {
 
     let karmaConfig = getKarmaConfig(karma, {
         basePath: __dirname,
-        webpack:  WEBPACK_CONFIG
+        webpack:  getWebpackConfig()
     });
 
     karma.set(karmaConfig);
