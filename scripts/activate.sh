6279e2ea9255f19e1f4955bb28d965969d96ac9c
Daniel Brain <dbrain@paypal.com>
2018-04-12 16:00:00 -0700

    Allow activate script to specify env

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -5,6 +5,7 @@ set -e;
 module="paypal-braintree-web-sdk";
 version="$1";
 tag="active";
+defenvs="local stage sandbox production";
 
 if [ -z "$version" ]; then
     version=$(npm view $module version);
@@ -13,5 +14,22 @@ fi;
 read -r -p "Upgrade $module to $version? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-    npm dist-tag add $module@$version $tag;
+
+
+    if [ -z "$2" ]; then
+        envs="$defenvs"
+    else
+        envs="$2"
+
+        for env in $envs; do
+            if [[ $env != "local" && $env != "stage" && $env != "standbox" && $env != "production" ]]; then
+                echo "Invalid env: $envs"
+                exit 1;
+            fi;
+        done;
+    fi;
+
+    for env in $envs; do
+        npm dist-tag add $module@$version "$tag-$env";
+    done;
 fi
