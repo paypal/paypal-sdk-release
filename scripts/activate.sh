59203f44a648d4afed289eea56aecbbe79d3dc89
Daniel Brain <dbrain@paypal.com>
2018-04-13 14:52:07 -0700

    Activate test version

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -5,30 +5,28 @@ set -e;
 module="paypal-braintree-web-sdk";
 version="$1";
 tag="active";
-defenvs="local stage sandbox production";
+defenvs="test local stage sandbox production";
 
 if [ -z "$version" ]; then
     version=$(npm view $module version);
 fi;
 
-read -r -p "Upgrade $module to $version? [y/N] " response
+if [ -z "$2" ]; then
+    envs="$defenvs"
+else
+    envs="$2"
+
+    for env in $envs; do
+        if [[ $env != "local" && $env != "stage" && $env != "standbox" && $env != "production" && $env != "test" ]]; then
+            echo "Invalid env: $envs"
+            exit 1;
+        fi;
+    done;
+fi;
+
+read -r -p "Upgrade $module to $version for env $envs? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-
-
-    if [ -z "$2" ]; then
-        envs="$defenvs"
-    else
-        envs="$2"
-
-        for env in $envs; do
-            if [[ $env != "local" && $env != "stage" && $env != "standbox" && $env != "production" ]]; then
-                echo "Invalid env: $envs"
-                exit 1;
-            fi;
-        done;
-    fi;
-
     for env in $envs; do
         npm dist-tag add $module@$version "$tag-$env";
     done;
