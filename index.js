2a2b0c741f2c111e415635ba1418e73a4a438970
Daniel Brain <dbrain@paypal.com>
2019-02-15 17:01:28 -0800

    Use sdk-client for setup logic

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
 /* @flow */
 
-type Component<T> = {
-    name : string,
-    requirer : () => T,
-    setupHandler : ?string
-};
-
-export default function setupSDK(namespace : string, version : string, components : $ReadOnlyArray<Component<mixed>>) {
-
-    if (window[namespace]) {
-        let { version: existingVersion } = window[namespace];
-        throw new Error(`SDK already loaded with ${ (existingVersion === version) ? 'same' : 'different' } version: ${ existingVersion } (this version: ${ version })`);
-    }
-
-    window[namespace] = window[namespace] || {};
-    window[namespace].version = version;
-
-    for (const { name, requirer, setupHandler } of components) {
-        let componentExports = {};
-
-        try {
-            // $FlowFixMe
-            let { [setupHandler]: setupComponent, ...rest } = requirer();
-
-            if (setupComponent) {
-                setupComponent();
-            }
-
-            componentExports = rest;
-        } catch (err) {
-            setTimeout(() => {
-                throw new Error(`Bootstrap Error for ${ name }:\n\n${ err.message }\n\n${ err.stack }`);
-            }, 1);
-            continue;
-        }
-
-        window[namespace] = {
-            ...window[namespace],
-            ...componentExports
-        };
-    }
-}
+import { setupSDK } from '@paypal/sdk-client/src';
+export default setupSDK;
