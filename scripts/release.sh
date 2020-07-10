00fa843a74c7d8cb955fbd8801ecdda572b283c0
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-07-10 14:07:10 -0500

    Validate npm user account before attempting to publish

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,6 +5,25 @@ set -e;
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 $DIR/validate.sh;
 
+if npm whoami &> /dev/null; then
+    echo "npm username: $(npm whoami)"
+else
+    echo "You must be logged in to publish a release. Running 'npm login'"
+    npm login
+fi
+
+USER_ROLE=$(npm org ls paypal "$(npm whoami)" --json)
+
+PERMISSION=$(node --eval "
+    let userRole = $USER_ROLE;
+    console.log(userRole['$(npm whoami)']);
+" "$USER_ROLE")
+
+if [ ! "$PERMISSION" = "developer" ]; then
+    echo "ERROR: You must be assigned the developer role in the npm paypal org to publish"
+    exit 1;
+fi
+
 npm version patch;
 
 git push;
