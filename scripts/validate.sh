75cc563afde3f95143f9486e4501f98ba72bdc71
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-07-10 10:39:48 -0500

    Pull the correct upstream commit when validating

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -3,31 +3,32 @@
 set -e;
 
 if ! git diff-files --quiet; then
-    echo "Can not continue with unstaged uncommited changes";
+    echo "ERROR: Can not continue with unstaged changes";
     exit 1;
 fi;
 
 if ! git diff-index --quiet --cached HEAD; then
-    echo "Can not continue with staged uncommited changes";
+    echo "ERROR: Can not continue with uncommitted changes";
     exit 1;
 fi;
 
 npx check-node-version --node='>=12' --npm='>=6.14';
 
 UPSTREAM='origin'
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM" "$( git rev-parse --abbrev-ref HEAD )" | tail -1)
-BASE=$(git merge-base @ "$UPSTREAM" "$( git rev-parse --abbrev-ref HEAD )")
+REMOTE=$(git rev-parse "$UPSTREAM"/"$BRANCH")
+BASE=$(git merge-base @ "$UPSTREAM"/"$BRANCH")
 
 if [ $LOCAL != $REMOTE ]; then
     if [ $LOCAL = $BASE ]; then
-        echo "Local repo behind upstream"
+        echo "ERROR: Local repo behind upstream"
         exit 1;
     elif [ $REMOTE = $BASE ]; then
-        echo "Local repo ahead of upstream"
+        echo "ERROR: Local repo ahead of upstream"
         exit 1;
     else
-        echo "Local repo diverged from upstream"
+        echo "ERROR: Local repo diverged from upstream"
         exit 1;
     fi
 fi
