29403d50e406396139c4e1f6f51754ce18bd41c2
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-12-17 13:53:29 -0600

    Fix flow error

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 import { setupSDK, type SetupComponent } from '@paypal/sdk-client/src';
 
+// $FlowFixMe
 export default (namespace : string, verison : string, components : $ReadOnlyArray<SetupComponent<mixed>>) => {
     return setupSDK(components);
 };
