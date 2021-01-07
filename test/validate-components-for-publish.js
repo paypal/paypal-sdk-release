2f324d9ea8183bbc175ac2f3b398a65369fab431
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-01-07 16:30:26 -0600

    Test with prepublish (#18)

---
+++ test/validate-components-for-publish.js
@@ -0,0 +1,20 @@
+/* @flow */
+
+import pkg from '../package.json';
+import pkgLock from '../package-lock.json';
+
+// loop through all the @paypal dependencies and ensure their dependencies are included in package.json
+const pkgDependencies = Object.keys(pkg.dependencies);
+
+for (const dependencyName of pkgDependencies) {
+    if (dependencyName.includes('@paypal')) {
+        for (const subDependencyName of Object.keys(pkgLock.dependencies[dependencyName].requires)) {
+            if (!pkgDependencies.includes(subDependencyName)) {
+                throw new Error(`Expected package.json to include ${ subDependencyName } because its a dependency of ${ dependencyName }`);
+            }
+        }
+
+        // eslint-disable-next-line no-console
+        console.log(`Validated ${ dependencyName }`);
+    }
+}
