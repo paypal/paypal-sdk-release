890bddffe817d62a3f4a60d4db1e79e87f982a5f
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-01-07 16:27:47 -0600

    Fix linting errors

--- test/validate-components-for-publish.js
+++ test/validate-components-for-publish.js
@@ -3,13 +3,11 @@
 import pkg from '../package.json';
 import pkgLock from '../package-lock.json';
 
-async function validateComponents() : Promise<void> {
-    // loop through all the @paypal dependencies and ensure their dependencies are included in package.json
-    const pkgDependencies = Object.keys(pkg.dependencies);
+// loop through all the @paypal dependencies and ensure their dependencies are included in package.json
+const pkgDependencies = Object.keys(pkg.dependencies);
 
-    for (const dependencyName of pkgDependencies) {
-        if (!dependencyName.includes('@paypal')) return;
-
+for (const dependencyName of pkgDependencies) {
+    if (dependencyName.includes('@paypal')) {
         for (const subDependencyName of Object.keys(pkgLock.dependencies[dependencyName].requires)) {
             if (!pkgDependencies.includes(subDependencyName)) {
                 throw new Error(`Expected package.json to include ${ subDependencyName } because its a dependency of ${ dependencyName }`);
@@ -20,10 +18,3 @@ async function validateComponents() : Promise<void> {
         console.log(`Validated ${ dependencyName }`);
     }
 }
-
-validateComponents().catch(err => {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    // eslint-disable-next-line no-process-exit, unicorn/no-process-exit
-    process.exit(1);
-});
