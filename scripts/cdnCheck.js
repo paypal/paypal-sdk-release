3ec8e7b51bbc7feb996e3fe893a36f71aa0052e5
Prashant <pjagasia@paypal.com>
2023-02-02 13:50:05 -0800

    review comments addressed

--- scripts/cdnCheck.js
+++ scripts/cdnCheck.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
-import { getPackage } from "./utils";
+import { dependencies } from "../package-lock.json";
 
 async function cdnCheck () {
-  const version = getPackage().dependencies["@paypal/sdk-logos"].version;
-
+  const version = dependencies["@paypal/sdk-logos"].version;
+console.log(version);
   fetch(`https://www.paypalobjects.com/js-sdk-logos/${version}/ideal-black.svg`)
   .then((response) => {
     console.log(response.status);
-    if (!(response.status >= 200 && response.status <= 300)) {
+    if (!(response.status >= 200 && response.status < 300)) {
       throw new Error ("Error retreiving svg")
     }
   })
--- scripts/utils.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs-extra";
-
-export function getPackage() {
-  if (!fs.existsSync("./package-lock.json")) {
-    throw new Error(`Package file not found`);
-  }
-  return JSON.parse(fs.readFileSync("./package-lock.json", "utf-8"));
-}
