3f683f9b056ca647125b12313f56d00e61a3a50c
Dustin Jones <dustintaylorjones@gmail.com>
2023-02-08 11:48:49 -0500

    chore: update console logs

--- scripts/sdk-logos-cdn-check.js
+++ scripts/sdk-logos-cdn-check.js
@@ -3,10 +3,13 @@ import { dependencies } from "../package-lock.json";
 
 async function cdnCheck () {
   const version = dependencies["@paypal/sdk-logos"].version;
-console.log(version);
-  await fetch(`https://www.paypalobjects.com/js-sdk-logos/${version}/paypal-default.svg`)
+  console.log(`Paypal SDK Logos v${version} installed`);
+
+  const url = `https://www.paypalobjects.com/js-sdk-logos/${version}/paypal-default.svg`
+  console.log(`Attempting to fetch ${url}`)
+  await fetch(url)
   .then((response) => {
-    console.log(response.status);
+    console.log(`Response status: ${response.status}`);
     if (!(response.status >= 200 && response.status < 300)) {
       throw new Error ("Error retreiving svg")
     }
