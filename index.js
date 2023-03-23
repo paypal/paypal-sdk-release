a773bfc1275e507557e8328f11caab725231b9c2
Oscar Leon <oscarleonnogales@gmail.com>
2023-03-23 16:12:07 -0500

    flow issue

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 
 import { setupSDK, type SetupComponent } from "@paypal/sdk-client/src";
 
-// $FlowFixMe
 export default (
   namespace: string,
   verison: string,
   components: $ReadOnlyArray<SetupComponent<mixed>>
+  // $FlowFixMe
 ) => {
   return setupSDK(components);
 };
