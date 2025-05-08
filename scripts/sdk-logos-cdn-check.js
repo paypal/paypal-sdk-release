8fb00a9d542e1ade58bd6a6197f85ef59a5988b8
nroman <nroman@paypal.com>
2025-05-08 13:39:48 -0500

    update node and  actions

--- scripts/sdk-logos-cdn-check.js
+++ scripts/sdk-logos-cdn-check.js
@@ -1,16 +1,16 @@
 import fetch from "node-fetch";
+
 import { dependencies } from "../package-lock.json";
 
 async function cdnCheck() {
   const version = dependencies["@paypal/sdk-logos"].version;
   console.log(`Paypal SDK Logos v${version} installed`);
-
   const url = `https://www.paypalobjects.com/js-sdk-logos/${version}/paypal-default.svg`;
   console.log(`Attempting to fetch ${url}`);
   await fetch(url).then((response) => {
     console.log(`Response status: ${response.status}`);
     if (!(response.status >= 200 && response.status < 300)) {
-      throw new Error("Error retreiving svg");
+      throw new Error("Error retrieving svg");
     }
   });
 }
